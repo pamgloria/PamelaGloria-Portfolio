4632b5afa5918e66d36d52e881c7d07a7832c0e5
Pam <pamela.o.gloria@gmail.com>
2022-08-14 10:31:35 -0700

    added all portfolio files into new PamelaGloria-Portfolio repo

---
+++ javascript/contact.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".auto-typed", {
+    strings: ["Thank you", "Gracias", "شکریہ", "Дякую", "Mahalo", "謝謝", "Diolch", "متشکرم", "Vielen Dank", "ขอขอบคุณ", "Баярлалаа" ],
+    typedSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".auto-typed", {
+    strings: ["Hello", "Hola", "नमस्ते", "привіт", "Kamusta", "你好", "أهلا", "habari", "bonjour", "Ahoj", "سلام" ],
+    typedSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
