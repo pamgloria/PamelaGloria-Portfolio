e6a568a4e2514a8b792a7729e7082ef5d5b882a6
Pam <pamela.o.gloria@gmail.com>
2022-08-14 17:42:08 -0700

    fixed sizing/flexbox for index & projects, lowercased words on main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,5 @@
 let typed = new Typed(".auto-typed", {
-    strings: ["Hello", "Hola", "नमस्ते", "привіт", "Kamusta", "你好", "أهلا", "habari", "bonjour", "Ahoj", "سلام" ],
+    strings: ["hello", "hola", "नमस्ते", "привіт", "Kamusta", "你好", "أهلا", "habari", "bonjour", "Ahoj", "سلام" ],
     typedSpeed: 150,
     backSpeed: 150,
     loop: true
